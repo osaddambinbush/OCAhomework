2a733b7122951d8ec76c6d087c21693605113367
CC-Student <CC-Student@192.168.0.192>
2020-03-09 15:07:26 +0100

    commit

--- src/ueb/Aufgabe2.java
+++ src/ueb/Aufgabe2.java
@@ -12,33 +12,36 @@ public class Aufgabe2 {
 		boolean doppelt;
 
 		schleife1: for (int i = 0; i < zahlen1.length; i++) {
-			do {
-				zahlen1[i] = r.nextInt(50);
-				doppelt = false;
 
-				for (int j = 0; j < zahlen2.length; j++) {
-					zahlen2[j] = r.nextInt(50);
+			for (int j = 0; j < zahlen2.length; j++) {
+				zahlen2[j] = r.nextInt(50);
+				doppelt = false;
+				do {
+					zahlen1[i] = r.nextInt(50);
 					doppelt = false;
+
 					if (zahlen2[j] == zahlen1[i]) {
 						doppelt = true;
 						System.out.println("*************TREFFER****************");
 						System.out.println("Array 1: " + zahlen1[i]);
 						System.out.println("Array 2: " + zahlen2[j]);
 						System.out.println("*************TREFFER****************");
-						
+
 						break schleife1;
-						
-					}else {
+
+					} else {
 						System.out.println("*****************************");
 						System.out.println("Array 1: " + zahlen1[i]);
 						System.out.println("Array 2: " + zahlen2[j]);
 						System.out.println("*****************************");
+
+						continue;
+
 					}
 
-				}
-			} while (doppelt);
+				} while (doppelt);
 
-		}
+			}
 //		for (int array1 : zahlen1) {
 //
 //			for (int array2 : zahlen2) {
@@ -50,6 +53,7 @@ public class Aufgabe2 {
 //
 //		}
 
-	}
+		}
 
+	}
 }
