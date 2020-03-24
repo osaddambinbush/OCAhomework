c8a66192a014fdee2375ecb27fbc87b924283cd9
Joschka Bienhüls <joschka.bienhuels@gmail.com>
2020-03-24 08:25:20 +0100

    commit

---
+++ src/ueb/PwPrüfen.java
@@ -0,0 +1,60 @@
+package ueb;
+
+import javax.swing.JOptionPane;
+
+public class PwPrüfen {
+
+	public static void main(String[] args) {
+
+		String pw = JOptionPane.showInputDialog(null, "Bitte PW eingeben", "Magic PasswordChecker",
+				JOptionPane.PLAIN_MESSAGE);
+		boolean kleinbuchstabe = false;
+		boolean grossbuchstabe = false;
+	    boolean ziffern = false;
+		boolean sonderzeichen = false;
+
+		if (pw.length() >= 8) {
+
+			for (int i = 0; i < pw.length(); i++) {
+
+				char ch = pw.charAt(i);
+				if (Character.isLetter(ch)) {
+					if (Character.isLowerCase(ch)) {
+						kleinbuchstabe=true;
+					} else {
+						grossbuchstabe=true;
+					}
+
+				} else {
+					if (Character.isDigit(ch)) {
+						ziffern=true;
+					} else {
+						sonderzeichen=true;
+					}
+				}
+			}
+
+			if (kleinbuchstabe && grossbuchstabe && ziffern && sonderzeichen) {
+				JOptionPane.showMessageDialog(null, "Sie haben ein gültiges Passwort eingegeben", "Sicheres Passwort",
+						JOptionPane.WARNING_MESSAGE);
+
+				System.exit(0);
+			} else {
+				
+				JOptionPane.showMessageDialog(null, "Bitte gültiges Passwort eingeben", "Passwortanforderungen nicht erfüllt",
+						JOptionPane.WARNING_MESSAGE);
+
+				System.exit(0);
+			}
+
+		} else {
+			JOptionPane.showMessageDialog(null, "Bitte gültiges Passwort eingeben", "zu kurzes Passwort",
+					JOptionPane.WARNING_MESSAGE);
+
+			System.exit(0);
+		}
+
+	
+	}
+
+}
