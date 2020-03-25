ac28b17cf7103108d7fd0d05601284af401ed82e
Joschka Bienhüls <joschka.bienhuels@gmail.com>
2020-03-25 08:12:01 +0100

    Commit

--- src/ueb/AlInt.java
+++ src/ueb/AlInt.java
@@ -6,29 +6,19 @@ import java.util.Collections;
 public class AlInt {
 
 	public static void main(String[] args) {
-//		ArrayList<Integer> listInt = new ArrayList<Integer>(99);
-//		
-//		for(int i=0;i<=99; i++) {
-//			
-//		listInt.add(i);
-//		System.out.println(listInt);
-	}
 
-	{
 		ArrayList<String> birds = new ArrayList<String>();
 
-		birds.add("hawk");
+		birds.add("hawk"); // Falke
 		birds.add("hawk");
 		birds.add("robin");
 		birds.add("hawk");
 		birds.add("hawk");
-
 		if (birds.size() > 0) {
-
-			birds.removeAll(Collections.singleton("hawk"));
+			birds.removeAll(Collections.singleton("hawk")); // OCP
 			System.out.println(birds);
 
 		}
-	}
 
+	}
 }
