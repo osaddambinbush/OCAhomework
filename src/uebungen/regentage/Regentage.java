0822da6e2597544e58da262dd556901804ae5ec9
Joschka Bienhüls <joschka.bienhuels@gmail.com>
2020-04-20 10:58:55 +0200

    commit

--- src/uebungen/regentage/Regentage.java
+++ src/uebungen/regentage/Regentage.java
@@ -14,8 +14,8 @@ public class Regentage {
 //  Original-Daten (Achtung: Zahlen sind f. alle 40 Orte doppelt)
 // Erweitern Sie den Code mit einer Suchfunktion
 
-		String orte = "Halle an der SaaleKölnFreiburgEssenMülheimWuppertalBielefeldBochumBonnRegensburgNürnbergKielHamburgMünchenKoblenzGöttingenDüsseldorfDortmundAugsburgHannoverSchwerinSaarbrückenPotsdamDresdenDarmstadtWiesbadenMainzFrankfurt am MainLeipzigMagdeburgErfurtWeimarStuttgartFrankfurt an der OderBremenBerlinKarlsruheMönchengladbachMoersTrier";
-		String rDays = "266266263263218218208208208208206206205205202202201201201201201201195195195195193193193193193193185185185185184184182182181181180180179179179179177177176176176176173173167167166166166166166166166166165165163163148148139139107107107107107107";
+		String orte = "Halle an der SaaleKölnFreiburgEssenMülheimWuppertalBielefeldBochumBonnRegensburgNürnbergKielHamburgMünchenKoblenzGöttingenDüsseldorfDortmundAugsburgHannoverSchwerinSaarbrückenPotsdamDresdenDarmstadtWiesbadenMainzFrankfurt am MainLeipzigMagdeburgErfurtWeimarStuttgartFrankfurt an der OderBremenBerlinKarlsruheMönchengladbachMoersTrierEnd";
+		String rDays = "266266263263218218208208208208206206205205202202201201201201201201195195195195193193193193193193185185185185184184182182181181180180179179179179177177176176176176173173167167166166166166166166166166165165163163148148139139107107107107107107EndEnd";
 		String ort = "";
 		StringBuilder sb = new StringBuilder(rDays);
 		StringBuilder sb1 = new StringBuilder(orte);
@@ -28,11 +28,12 @@ public class Regentage {
 
 			char ch = sb1.charAt(i);
 			char ch1 = sb1.charAt(i - 1);
-			if (!Character.isSpaceChar(ch1) & Character.isUpperCase(ch)) {
+			if (!Character.isSpaceChar(ch1) & Character.isUpperCase(ch) ) {
 				ort = sb1.substring(0, i);
 				sb1.delete(0, i);
 				aLo1.add(ort);
 				i = 1;
+			
 			}
 
 		}
@@ -45,15 +46,16 @@ public class Regentage {
 				aL1.add(i, rain);
 			}
 		}
-		for (int i = 0; i < aL1.size() - 1; i++)
-			System.out.println(aLo1.get(i) + " hatte im Jahr 2004 '" + aL1.get(i) + "' Regentage.");
+		for (int i = 0; i < aL1.size() -1; i++)
+			System.out.println( "Nr: " +(i+1)+ " "+ aLo1.get(i) + " hatte im Jahr 2004 '" + aL1.get(i) + "' Regentage.");
 
 		String search = JOptionPane.showInputDialog(null, "Bitte die Stadt eingeben, die Sie suchen:", "Städtesuche",
 				JOptionPane.PLAIN_MESSAGE);
 
 		for (int i = 0; i < aLo1.size(); i++)
-			if (search.equals(aLo1.get(i)))
+			if (search !=null & search.equals(aLo1.get(i))) 
 				System.err.println("Suchergebnis: " + aLo1.get(i) + " hat " + aL1.get(i) + " Regentage (Stand: 2004)");
+			
 
 	}
 
